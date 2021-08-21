22cd99a01d8d2368767f6e0e2e40c73b59f1608b
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-08-21 19:02:07 +0530

    UC9-DictEmpWage

---
+++ empWage9.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+MaxHrsInMonth=4
+EmpRatePerHr=20
+numOfWorkingDays=20
+
+TotalEmpHrs=0
+TotalWorkingDays=0
+
+declare -A dailyWage;
+
+function getWorkingHrs()
+{
+local $empCheck=$1
+case $empCheck in
+   $isFullTime)
+      empHrs=8
+         ;;
+   $isPartTime)
+      empHrs=4
+         ;;
+*)
+empHrs=0
+;;
+esac
+echo $empHrs
+}
+function getEmpWage()
+{
+local $empHrs=$1
+echo $(($empHrs+$EmpRatePerHr))
+}
+while [[ $TotalEmpHrs -lt $MaxHrsInMonth &&
+         $TotalWorkingDays -lt $numOfWorkingDays ]]
+do
+   ((TotalWorkingDays++))
+   empCheck=$((RANDOM%3))
+   empHrs="$( getWorkingHrs $empCheck )"
+   TotalEmpHrs=$(($TotalEmpHrs+$empHrs))
+   dailyWage["Day" $TotalWorkingDays]="$( getEmpWage $empHrs )"
+done
+totalSalary=$(($TotalEmpHrs+$EmpRatePerHr))
+echo ${dailyWage[@]}
+echo ${!dailyWage[@]}

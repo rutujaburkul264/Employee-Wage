ac9c0f6fe08d674e44e46d114b02f7ab3c1eb4bf
rutujaburkul264 <rutujaburkul264@gmail.com>
2021-08-21 10:40:46 +0530

    Added:EmpWageComputation

--- empWageCompProb.sh
+++ empWageCompProb.sh
@@ -1,1 +1,11 @@
-Welcome to Employee Wage Computation Problem
+#!/bin/bash -x
+
+isPresent=1
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
